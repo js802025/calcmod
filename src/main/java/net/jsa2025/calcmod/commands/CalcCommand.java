aca6975bf727131e443758d3053e6090c5894d4f
js802025 <30957566+js802025@users.noreply.github.com>
2022-08-02 17:51:43 -0500

    added click to copy, help
    Changed /calc basic to /calc

--- src/main/java/net/jsa2025/calcmod/commands/CalcCommand.java
+++ src/main/java/net/jsa2025/calcmod/commands/CalcCommand.java
@@ -1,6 +1,7 @@
 package net.jsa2025.calcmod.commands;
 
 import com.ibm.icu.text.DecimalFormat;
+import com.ibm.icu.text.NumberFormat;
 import com.mojang.brigadier.CommandDispatcher;
 
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
@@ -20,10 +21,14 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 
 import static dev.xpple.clientarguments.arguments.CBlockPosArgumentType.*;
 
+import java.util.Locale;
+
 public class CalcCommand {
     static DecimalFormat df = new DecimalFormat("#.##");
+    static NumberFormat nf = NumberFormat.getInstance(new Locale("en", "US")); 
     public static void register (CommandDispatcher<FabricClientCommandSource> dispacther, CommandRegistryAccess registry) {
-        dispacther.register(ClientCommandManager.literal("calc").then(ClientCommandManager.literal("basic").then(ClientCommandManager.argument("expression", StringArgumentType.string()).executes(ctx -> executeBasicCalculation(ctx.getSource(), StringArgumentType.getString(ctx, "expression")))))
+        dispacther.register(ClientCommandManager.literal("calc")
+        .then(ClientCommandManager.argument("expression", StringArgumentType.greedyString()).executes(ctx -> executeBasicCalculation(ctx.getSource(), StringArgumentType.getString(ctx, "expression"))))
         .then(ClientCommandManager.literal("storage").then(ClientCommandManager.argument("itemsperhour", IntegerArgumentType.integer())
         .executes(ctx -> executeStorageCalculation(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "itemsperhour"), 1))
         .then(ClientCommandManager.argument("timesHopperSpeed", IntegerArgumentType.integer())
@@ -40,6 +45,10 @@ public class CalcCommand {
         .executes(ctx -> executeSecondsToHopperClock(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "seconds")))))
         .then(ClientCommandManager.literal("SecondsToRepeater").then(ClientCommandManager.argument("seconds", IntegerArgumentType.integer())
         .executes(ctx -> executeSecondsToRepeater(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "seconds")))))
+        .then(ClientCommandManager.literal("itemtostack").then(ClientCommandManager.argument("numberofitems", IntegerArgumentType.integer())
+        .executes(ctx -> executeItemToStack(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "numberofitems")))))
+        .then(ClientCommandManager.literal("help")
+        .executes(ctx -> executeHelp(ctx.getSource())))
 
         );
 
@@ -48,7 +57,8 @@ public class CalcCommand {
 
     public static int executeBasicCalculation(FabricClientCommandSource source, String expression) {
         Expression e = new Expression(expression);
-        source.getPlayer().sendMessage(Text.of("Result: " + e.calculate()));
+        String message = "Result: " + nf.format(e.calculate());
+        source.getPlayer().sendMessage(Text.literal(message).append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, message)))));
         return 1;
         
     }
@@ -57,37 +67,44 @@ public class CalcCommand {
         double hopperSpeed = (9000*timesHopperSpeed);
         double sorters = Math.ceil(itemsperhour/hopperSpeed);
         double sbsperhour = itemsperhour * 1.0 / 1728;
-
+        String message = "Required Sorters at "+timesHopperSpeed+"xHopper Speed(9000/h): " + nf.format(sorters) + "\nSbs per hour: " +nf.format(sbsperhour);
         
-        source.getPlayer().sendMessage(Text.of("Required Sorters at "+timesHopperSpeed+"xHopper Speed(9000/h): " + sorters + "\nSbs per hour: " +df.format(sbsperhour)));
+        source.getPlayer().sendMessage(Text.literal(message).append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, message)))));
+
         return 1;
     }
 
     public static int executeNetherCoord(FabricClientCommandSource source, BlockPos pos ) {
-        source.getPlayer().sendMessage(Text.literal("Nether Coord: X: " + pos.getX()/8 + " Z: " + pos.getZ()/8 + " ").append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, "X: " + pos.getX()/8 + " Z: " + pos.getZ()/8)))));
+        source.getPlayer().sendMessage(Text.literal("Nether Coord: X: " + nf.format(pos.getX()/8) + " Z: " + nf.format(pos.getZ()/8) + " ").append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, "X: " + nf.format(pos.getX()/8) + " Z: " + nf.format(pos.getZ()/8))))));
         return 1;
     }
 
     public static int executeOverworldCoord(FabricClientCommandSource source, BlockPos pos ) {
-        source.getPlayer().sendMessage(Text.literal("Overworld Coord: X: " + pos.getX()*8 + " Z: " + pos.getZ()*8 +" ").append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, "X: " + pos.getX()/8 + " Z: " + pos.getZ()/8)))));
+        source.getPlayer().sendMessage(Text.literal("Overworld Coord: X: " + nf.format(pos.getX()*8) + " Z: " + nf.format(pos.getZ()*8) + " ").append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, "X: " + nf.format(pos.getX()*8) + " Z: " + nf.format(pos.getZ()*8))))));
         return 1;
     }
 
     public static int executeSbToItem(FabricClientCommandSource source, int numberofsbs) {
         double items = numberofsbs * 1728;
-        source.getPlayer().sendMessage(Text.of("Items per SB: " + items));
+        String message = "Items per SB: " + nf.format(items);
+        source.getPlayer().sendMessage(Text.literal(message).append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, message)))));
+
         return 1;
     }
 
     public static int executeItemToSb(FabricClientCommandSource source, int numberofitems) {
         double sbs = numberofitems / 1728;
-        source.getPlayer().sendMessage(Text.of("Sbs per Item: " + df.format(sbs)));
+        String message = "Sbs per Item: " + nf.format(sbs);
+
+
+       source.getPlayer().sendMessage(Text.literal(message).append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, message)))));
         return 1;
     }
 
     public static int executeSecondsToHopperClock(FabricClientCommandSource source, int seconds) {
-        double hopperclock = Math.ceil(seconds *2.5);
-        source.getPlayer().sendMessage(Text.of("Items Required: " + hopperclock));
+        double hopperclock = Math.ceil(seconds *1.25);
+        String message = "Hopper Clock: " + nf.format(hopperclock);
+        source.getPlayer().sendMessage(Text.literal(message).append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, message)))));
         return 1;
     }
 
@@ -95,10 +112,55 @@ public class CalcCommand {
         int ticks = seconds * 10;
         double repeaters = Math.ceil(ticks/4);
         if (ticks % 4 != 0) {
-            source.getPlayer().sendMessage(Text.of("Repeaters Required: " + repeaters +"\nLast Repeater Tick: "+ticks % 4 ));
+            String message = "Repeaters Required: " + nf.format(repeaters) +"\nLast Repeater Tick: "+ nf.format(ticks % 4) ;
+            source.getPlayer().sendMessage(Text.literal(message).append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, message)))));
+
         } else {
-            source.getPlayer().sendMessage(Text.of("Repeaters Required: " + Math.ceil(ticks/4)));
+            String message = "Repeaters Required: " + nf.format(Math.ceil(ticks/4));
+            source.getPlayer().sendMessage(Text.literal(message).append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, message)))));
+
         }
+        return 1;
+    }
+    
+    public static int executeItemToStack(FabricClientCommandSource source, int items) {
+        double stacks = Math.floor(items/64);
+        int leftover = items % 64;
+        String message = "Stacks Required: " + nf.format(stacks) + "\nLeftover Items: " + nf.format(leftover);
+        source.getPlayer().sendMessage(Text.literal(message).append(Text.literal("\2473[Click To Copy]").setStyle(Style.EMPTY.withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, message)))));
+        return 1;
+    }
+
+    public static int executeHelp(FabricClientCommandSource source) {
+        var helpMessage = """
+            Calcmod 
+
+            Basic(no arguments):
+            Functions like a simple calculator 
+            
+            Storage:
+            Given rate in terms of items per hour and optionally hopper speed returns the number of needed sorters and rates in terms of sbs per hour
+            
+            Nether:
+            Returns coords in nether for building portal in correct place.
+            
+            Overworld:
+            Same as nether but reversed 
+            
+            sbtoitem:
+            Given number of sbs return number of items
+            
+            Itemtosb:
+            Given number of items return amount of sbs needed
+            
+            SecondstoHopperClock
+            Given seconds return amount of items needed in hopper clock
+            2.5 items per second
+            
+            SecondstoRepeaterClock
+            Given number of seconds return needed repeater delay 
+                """;;
+        source.getPlayer().sendMessage(Text.of(helpMessage));
         return 1;
     }
 
