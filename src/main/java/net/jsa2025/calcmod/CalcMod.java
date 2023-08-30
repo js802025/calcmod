fc812bc3367b7f808784781fcf6135382cf050ab
js802025 <30957566+js802025@users.noreply.github.com>
2023-08-30 16:03:44 -0500

    Update CalcMod.java

--- src/main/java/net/jsa2025/calcmod/CalcMod.java
+++ src/main/java/net/jsa2025/calcmod/CalcMod.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.jsa2025.calcmod.commands.CalcCommand;
 
+import java.util.logging.Logger;
 
 
 public class CalcMod implements DedicatedServerModInitializer, ClientModInitializer {
@@ -13,7 +14,7 @@ public class CalcMod implements DedicatedServerModInitializer, ClientModInitiali
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 
-
+	public static final Logger LOGGER = Logger.getLogger("calcmod");
 	@Override
 	public void onInitializeClient() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
