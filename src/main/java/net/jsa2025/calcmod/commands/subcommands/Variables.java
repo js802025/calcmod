eb6674634ded60db8db988ebd0fbb1e92939df9a
js802025 <30957566+js802025@users.noreply.github.com>
2023-09-22 22:07:01 -0500

    Update Variables.java

--- src/main/java/net/jsa2025/calcmod/commands/subcommands/Variables.java
+++ src/main/java/net/jsa2025/calcmod/commands/subcommands/Variables.java
@@ -32,7 +32,7 @@ public class Variables {
     }
 
     public static CalcMessageBuilder execute() {
-        String message = "§bVariables§f can be used inside equations in any number field. They act as shortcuts instead of having to remember that “a double chest full of 16 stackable items is 864.” \nIf no stack size is given, variables default to the stack size of each command.  \ndub: 3456 (default)  \ndub64: 3456  \ndub16: 864  \ndub1: 54  \nsb: 1728 (default)  \nsb64: 1728  \nsb16: 432  \nsb1: 27  \nstack: 64 (default)  \nstack64: 64  \nstack16: 16  \nstack1: 1  \nmin: 60  \nhour: 3600  \nx: player x coord  \ny: player y coord  \nz: player z coord ";
+        String message = "§bVariables§f can be used inside equations in any number field. They act as shortcuts instead of having to remember that “a double chest full of 16 stackable items is 864.” \nIf no stack size is given, variables default to the stack size of each command.  \ndub: 3456 (default)  \ndub64: 3456  \ndub16: 864  \ndub1: 54  \nsb: 1728 (default)  \nsb64: 1728  \nsb16: 432  \nsb1: 27  \nstack: 64 (default)  \nstack64: 64  \nstack16: 16  \nstack1: 1  \nmin: 60  \nhour: 3600  \nx: player x coord  \ny: player y coord  \nz: player z coord\n health: player health ";
         return new CalcMessageBuilder(message);
     }
 }
