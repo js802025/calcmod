dff8e1a65806e485b37e0484394e2f08964886a3
js802025 <30957566+js802025@users.noreply.github.com>
2025-03-28 21:22:02 -0500

    added 3d mode

--- src/main/java/net/jsa2025/calcmod/commands/subcommands/Distance.java
+++ src/main/java/net/jsa2025/calcmod/commands/subcommands/Distance.java
@@ -33,7 +33,23 @@ public class Distance {
             CalcCommand.sendMessage(ctx.getSource(), message);
             return 1;
                 }
-        )).then(ClientCommandManager.literal("help").executes(ctx -> {
+        )).then(ClientCommandManager.literal("3d").then(ClientCommandManager.argument("pos1", CBlockPosArgument.blockPos()).then(ClientCommandManager.argument("pos2", CBlockPosArgument.blockPos()).executes(
+                                ctx -> {
+                                    BlockPos pos1 = CBlockPosArgument.getBlockPos(ctx, "pos1");
+                                    BlockPos pos2 = CBlockPosArgument.getBlockPos(ctx, "pos2");
+                                    CalcMessageBuilder message = execute3d(pos1, pos2);
+                                    CalcCommand.sendMessage(ctx.getSource(), message);
+                                    return 1;
+                                }
+                        )).executes(ctx -> {
+                                    BlockPos pos1 = ctx.getSource().getPlayer().getBlockPos();
+                                    BlockPos pos2 = CBlockPosArgument.getBlockPos(ctx, "pos1");
+                                    CalcMessageBuilder message = execute3d(pos1, pos2);
+                                    CalcCommand.sendMessage(ctx.getSource(), message);
+                                    return 1;
+                                }
+                        )))
+                        .then(ClientCommandManager.literal("help").executes(ctx -> {
             CalcMessageBuilder message = Help.execute("dist");
             CalcCommand.sendMessage(ctx.getSource(), message);
             return 1;
@@ -59,7 +75,22 @@ public class Distance {
                     CalcCommand.sendMessageServer(ctx.getSource(), message);
                     return 1;
                 }
-        )).then(CommandManager.literal("help").executes(ctx -> {
+        )).then(CommandManager.literal("3d").then(CommandManager.argument("pos1", BlockPosArgumentType.blockPos()).then(CommandManager.argument("pos2", BlockPosArgumentType.blockPos()).executes(
+                ctx -> {
+                    BlockPos pos1 = BlockPosArgumentType.getBlockPos(ctx, "pos1");
+                    BlockPos pos2 = BlockPosArgumentType.getBlockPos(ctx, "pos2");
+                    CalcMessageBuilder message = execute3d(pos1, pos2);
+                    CalcCommand.sendMessageServer(ctx.getSource(), message);
+                    return 1;
+                }
+        )).executes(ctx -> {
+                    BlockPos pos1 = ctx.getSource().getPlayer().getBlockPos();
+                    BlockPos pos2 = BlockPosArgumentType.getBlockPos(ctx, "pos1");
+                    CalcMessageBuilder message = execute3d(pos1, pos2);
+                    CalcCommand.sendMessageServer(ctx.getSource(), message);
+                    return 1;
+                }
+        ))).then(CommandManager.literal("help").executes(ctx -> {
             CalcMessageBuilder message = Help.execute("dist");
             CalcCommand.sendMessageServer(ctx.getSource(), message);
             return 1;
@@ -68,15 +99,23 @@ public class Distance {
     }
 
     public static CalcMessageBuilder execute(BlockPos pos1, BlockPos pos2) {
-        double dist = Math.sqrt(Math.pow(pos1.getX()-pos2.getZ(), 2) + Math.pow(pos1.getY()-pos2.getZ(), 2));
+        double dist = Math.sqrt(Math.pow(pos1.getX()-pos2.getX(), 2) + Math.pow(pos1.getZ()-pos2.getZ(), 2));
         CalcMessageBuilder message = new CalcMessageBuilder().addString("Distance from ").addInput("X: "+pos1.getX()+" Z: "+pos1.getZ()).addString(" to ").addInput("X: "+pos2.getX()+" Z: "+pos2.getZ()).addString(": ").addResult(String.valueOf(nf.format(dist)));
+        return message;
+    }
+
+    public static CalcMessageBuilder execute3d(BlockPos pos1, BlockPos pos2) {
+        double dist = Math.sqrt(Math.pow(pos1.getX()-pos2.getX(), 2) + Math.pow(pos1.getY()-pos2.getY(), 2) + Math.pow(pos1.getZ()-pos2.getZ(), 2));
+        CalcMessageBuilder message = new CalcMessageBuilder().addString("Distance from ").addInput("X: "+pos1.getX()+" Y: "+pos1.getY()+" Z: "+pos1.getZ()).addString(" to ").addInput("X: "+pos2.getX()+" Y: "+pos2.getY()+" Z: "+pos1.getZ()).addString(": ").addResult(String.valueOf(nf.format(dist)));
         return message;
     }
 
     public static String helpMessage = """
         §b§LDistance:§r§f
-            Given two block positions, gives distance between them. If only one position is given, uses player's location.
+            Given two block positions, gives distance between them. If only one position is given, uses player's location. The 3D mode provides distance including height.
             §eUsage: /calc dist <x1> <y1>§f
             §eUsage: /calc dist <x1> <y1> <z1> <x2> <x2> <z2>§f
+            §eUsage: /calc dist 3d <x1> <y1>§f
+            §eUsage: /calc dist 3d <x1> <y1> <z1> <x2> <x2> <z2>§f
                 """;
 }
