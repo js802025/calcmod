5d9cfd4b5978e0f0e7b7d067d126b6602c8a989d
void <wordhh1@gmail.com>
2025-04-11 20:27:59 -0600

    Use the actual mod id for the logger instead of generic "modid"

--- src/main/java/net/jsa2025/calcmod/CalcMod.java
+++ src/main/java/net/jsa2025/calcmod/CalcMod.java
@@ -13,7 +13,7 @@ public class CalcMod implements ClientModInitializer, DedicatedServerModInitiali
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("calcmod");
 
 	@Override
 	
