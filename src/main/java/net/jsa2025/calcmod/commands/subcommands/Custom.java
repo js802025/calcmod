30fd83252f22ec1d5077bf9d1237b07d97e6eb14
js802025 <30957566+js802025@users.noreply.github.com>
2026-02-11 12:11:39 -0500

    prevent recursive custom functions

--- src/main/java/net/jsa2025/calcmod/commands/subcommands/Custom.java
+++ src/main/java/net/jsa2025/calcmod/commands/subcommands/Custom.java
@@ -45,9 +45,11 @@ public class Custom {
                                     String function = StringArgumentType.getString(ctx, "function");
                                     String name = StringArgumentType.getString(ctx, "name");
                                     CalcMessageBuilder messageBuilder;
-                                    if (!Pattern.matches(".*\\d.*", name) && !parseEquationVariables(function).isEmpty()) {
+                                    if (!Pattern.matches(".*\\d.*", name) && !parseEquationVariables(function).isEmpty() && !function.substring(function.indexOf('(') + 1).contains(name+"(")) {
                                         saveNewCommand(name, function);
-                                        messageBuilder = new CalcMessageBuilder("§eAdded "+name+"§f");
+                                        messageBuilder = new CalcMessageBuilder("<yellow>Added "+name+"<white>");
+                                    } else if (function.substring(function.indexOf('(') + 1).contains(name+"(")){
+                                        messageBuilder = new CalcMessageBuilder("§cCommand cannot call itself.§f");
                                     } else if (parseEquationVariables(function).isEmpty()) {
                                         messageBuilder = new CalcMessageBuilder("§cMust have at least one parameter.§f");
                                     }else {
@@ -105,9 +107,11 @@ public class Custom {
                                             String function = StringArgumentType.getString(ctx, "function");
                                             String name = StringArgumentType.getString(ctx, "name");
                                             CalcMessageBuilder messageBuilder;
-                                            if (!Pattern.matches(".*\\d.*", name) && !parseEquationVariables(function).isEmpty()) {
+                                            if (!Pattern.matches(".*\\d.*", name) && !parseEquationVariables(function).isEmpty() && !function.substring(function.indexOf('(') + 1).contains(name+"(")) {
                                                 saveNewCommand(name, function);
-                                                messageBuilder = new CalcMessageBuilder("§eAdded "+name+"§f");
+                                                messageBuilder = new CalcMessageBuilder("<yellow>Added "+name+"<white>");
+                                            } else if (function.substring(function.indexOf('(') + 1).contains(name+"(")){
+                                                messageBuilder = new CalcMessageBuilder("§cCommand cannot call itself.§f");
                                             } else if (parseEquationVariables(function).isEmpty()) {
                                                 messageBuilder = new CalcMessageBuilder("§cMust have at least one parameter.§f");
                                             }else {
